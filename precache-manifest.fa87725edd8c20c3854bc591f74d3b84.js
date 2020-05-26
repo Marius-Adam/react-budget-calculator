bc908ae9ed3f37fd2ae15d74fc8e60b55baaf8dd
Marius-Adam <marius.c.adam@gmail.com>
2020-05-26 17:20:31 +0100

    Updates

---
+++ precache-manifest.fa87725edd8c20c3854bc591f74d3b84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39aee45ec626d5efadc6a5cf00309e7",
+    "url": "/react-budget-calculator/index.html"
+  },
+  {
+    "revision": "eb89a2235cf93136e3aa",
+    "url": "/react-budget-calculator/static/css/main.535c005a.chunk.css"
+  },
+  {
+    "revision": "44f3d304bb45014d753d",
+    "url": "/react-budget-calculator/static/js/2.62938e40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-budget-calculator/static/js/2.62938e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb89a2235cf93136e3aa",
+    "url": "/react-budget-calculator/static/js/main.c1e343c1.chunk.js"
+  },
+  {
+    "revision": "0d1d6c000100d32a51b0",
+    "url": "/react-budget-calculator/static/js/runtime-main.26ffb01b.js"
+  }
+]);
